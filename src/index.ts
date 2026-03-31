15e5f623f93622e8dc15a317b4610a2b0aa68569
Moenarch <Moenarch>
2026-04-01 01:59:18 +0200

    advanced

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+"use client";
+
+export {
+  StoryContainer,
+  type StoryContainerProps,
+} from "./story-container";
+export { StoryScene } from "./story-scene";
+export { StorySeries, type StorySeriesProps } from "./story-series";
+export type {
+  StorySceneDefinition,
+  StorySceneProps,
+} from "./story-types";
